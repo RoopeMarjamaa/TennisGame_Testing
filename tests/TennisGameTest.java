a258f36237d577902410a55514fb8ee31f9c4386
RoopeMarjamaa <118440808+RoopeMarjamaa@users.noreply.github.com>
2023-11-07 23:15:18 +0200

    Update TennisGameTest.java

--- tests/TennisGameTest.java
+++ tests/TennisGameTest.java
@@ -1,19 +1,105 @@
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
-import java.lang.reflect.Method;
+import static org.junit.Assert.*;
+
 
 public class TennisGameTest {
-    private TennisGame game;
+	private TennisGame game;
+	
+	
+	
 
     @Before
     public void setUp() {
         game = new TennisGame();
     }
+	
+
+	@Test
+	public void testTennisGame_Start() {
+		//Arrange
+		TennisGame game = new TennisGame();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Initial score incorrect", "love - love", score);		
+	}
+	
+	@Test
+	public void testTennisGame_EahcPlayerWin4Points_Score_Deuce() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+		
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		game.player2Scored();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Tie score incorrect", "deuce", score);		
+	}
+	
+	
+	
+	@Test
+    public void testInitialScore() {
+        assertEquals("love - love", game.getScore());
+    }
 
     @Test
-    public void testInitialScore() {
-        assertEquals("Initial score should be 'love - love'", "love - love", game.getScore());
+    public void testPlayer1Scores() throws TennisGameException {
+        game.player1Scored();
+        assertEquals("15 - love", game.getScore());
+    }
+
+    @Test
+    public void testPlayer2Scores() throws TennisGameException {
+        game.player2Scored();
+        assertEquals("love - 15", game.getScore());
+    }
+
+    @Test
+    public void testDeuce() throws TennisGameException {
+        game.player1Scored();
+        game.player2Scored();
+        game.player1Scored();
+        game.player2Scored();
+        game.player1Scored();
+        game.player2Scored();
+        assertEquals("deuce", game.getScore());
+    }
+
+    @Test
+    public void testAdvantagePlayer1() throws TennisGameException {
+        game.player1Scored();
+        game.player2Scored();
+        game.player1Scored();
+        game.player2Scored();
+        game.player1Scored();
+        game.player2Scored();
+        game.player1Scored();
+        assertEquals("player1 has advantage", game.getScore());
+    }
+
+    @Test
+    public void testAdvantagePlayer2() throws TennisGameException {
+        game.player1Scored();
+        game.player2Scored();
+        game.player1Scored();
+        game.player2Scored();
+        game.player1Scored();
+        game.player2Scored();
+        game.player2Scored();
+        assertEquals("player2 has advantage", game.getScore());
     }
 
     @Test
@@ -22,8 +108,7 @@ public class TennisGameTest {
         game.player1Scored();
         game.player1Scored();
         game.player1Scored();
-
-        assertEquals("Player1 should win", "player1 wins", game.getScore());
+        assertEquals("player1 wins", game.getScore());
     }
 
     @Test
@@ -32,378 +117,14 @@ public class TennisGameTest {
         game.player2Scored();
         game.player2Scored();
         game.player2Scored();
-
-        assertEquals("Player2 should win", "player2 wins", game.getScore());
+        assertEquals("player2 wins", game.getScore());
     }
 
-    @Test
-    public void testGetScoreDeuce() throws TennisGameException {
-        game.player1Scored();
-        game.player2Scored();
-        game.player1Scored();
-        game.player2Scored();
-        game.player1Scored();
-        game.player2Scored();
-        game.player2Scored(); // Deuce
-
-        assertEquals("The score should be 'deuce'", "deuce", game.getScore());
-    }
-    
-    
-    @Test
-    public void testPlayer2Scored() throws TennisGameException {
-        game.player2Scored();
-        assertEquals("Score should be 'love - 15'", "love - 15", game.getScore());
-    }
-    
-    
-    
-    @Test
-    public void testGetScorePlayer1Wins() throws TennisGameException {
-        game.player1Scored();
-        game.player1Scored();
-        game.player1Scored();
-        game.player1Scored();
-        assertEquals("Player 1 should win", "player1 wins", game.getScore());
-    }
-    
-    
-    @Test
-    public void testGetScorePlayer2Advantage() throws TennisGameException {
-        game.player1Scored();
-        game.player1Scored();
-        game.player1Scored();
-        game.player2Scored();
-        game.player2Scored();
-        game.player2Scored();
-        game.player2Scored(); // Player 2 has advantage
-        assertEquals("Player 2 should have advantage", "player2 has advantage", game.getScore());
-    }
-
-    @Test
-    public void testGetScorePlayer1Advantage() throws TennisGameException {
-        game.player1Scored();
-        game.player1Scored();
-        game.player1Scored();
-        game.player2Scored();
-        game.player2Scored();
-        game.player2Scored();
-        game.player1Scored(); // Player 1 has advantage
-        assertEquals("Player 1 should have advantage", "player1 has advantage", game.getScore());
-    }
-
-    
-    @Test
-    public void testGetScoreLove() throws Exception {
-        Method getScoreMethod = TennisGame.class.getDeclaredMethod("getScore", int.class);
-        getScoreMethod.setAccessible(true);
-
-        String score = (String) getScoreMethod.invoke(game, 0);
-
-        assertEquals("0 points should be 'love'", "love", score);
-    }
-
-    @Test
-    public void testGetScoreFifteen() throws Exception {
-        Method getScoreMethod = TennisGame.class.getDeclaredMethod("getScore", int.class);
-        getScoreMethod.setAccessible(true);
-
-        String score = (String) getScoreMethod.invoke(game, 1);
-
-        assertEquals("1 point should be '15'", "15", score);
-    }
-
-    @Test
-    public void testGetScoreThirty() throws Exception {
-        Method getScoreMethod = TennisGame.class.getDeclaredMethod("getScore", int.class);
-        getScoreMethod.setAccessible(true);
-
-        String score = (String) getScoreMethod.invoke(game, 2);
-
-        assertEquals("2 points should be '30'", "30", score);
-    }
-
-    @Test
-    public void testGetScoreForty() throws Exception {
-        Method getScoreMethod = TennisGame.class.getDeclaredMethod("getScore", int.class);
-        getScoreMethod.setAccessible(true);
-
-        String score = (String) getScoreMethod.invoke(game, 3);
-
-        assertEquals("3 points should be '40'", "40", score);
-    }
-
-    @Test
-    public void testGetScoreDefault() throws Exception {
-        Method getScoreMethod = TennisGame.class.getDeclaredMethod("getScore", int.class);
-        getScoreMethod.setAccessible(true);
-
-        String score = (String) getScoreMethod.invoke(game, 4);
-
-        assertEquals("Any other point value should be '40'", "40", score);
-    }
-
-    
-    
    
 
-    @Test
-    public void testInvalidScoring() {
-        try {
-            game.player1Scored();
-            game.player1Scored();
-            game.player1Scored();
-            game.player1Scored();
-            game.player1Scored(); // This should throw an exception
-            fail("Expected TennisGameException");
-        } catch (TennisGameException e) {
-            // This is expected
-        }
-    }
-
-    
-    
-    
-    
-    
-    
-    
-    
    
     
     
-    @Test
-    public void testAdvantage() throws TennisGameException {
-        game.player1Scored();
-        game.player2Scored();
-        game.player1Scored();
-        game.player2Scored();
-        game.player1Scored();
-        game.player2Scored();
-        game.player1Scored();
-        game.player1Scored();
-
-       assertEquals("Player1 should have advantage", "player1 has advantage", game.getScore());
-    }
-
     
 
-    @Test
-    public void testPlayer2ScoredGameEnded() throws TennisGameException {
-        game.player1Scored();
-        game.player1Scored();
-        game.player1Scored();
-        game.player1Scored();
-        // Player 1 has won, the game is over
-
-        try {
-            game.player2Scored(); // This should throw an exception
-            fail("Expected TennisGameException");
-        } catch (TennisGameException e) {
-            // This is expected
-        }
-    }
-    
-    
-    @Test
-    public void testGetScorePlayer2Wins() throws TennisGameException {
-        game.player2Scored();
-        game.player2Scored();
-        game.player2Scored();
-        game.player2Scored();
-        assertEquals("Player 2 should win", "player2 wins", game.getScore());
-    }
-
-    
-    
-    
-    
-    @Test
-    public void testInvalidScoringAtDeuce() throws TennisGameException {
-        game.player1Scored();
-        game.player2Scored();
-        game.player1Scored();
-        game.player2Scored();
-        game.player1Scored();
-        game.player2Scored();
-
-        try {
-            game.player1Scored(); // This should throw an exception
-            fail("Expected TennisGameException");
-        } catch (TennisGameException e) {
-            // This is expected
-        }
-    }
 }
-
-
-
-
-
-
-
-
-/* Alkuperaiset testis 
- * 
- 
-
-    @Before
-    public void setUp() {
-        game = new TennisGame();
-    }
-
-    @Test
-    public void testInitialScore() {
-        assertEquals("Initial score should be 'love - love'", "love - love", game.getScore());
-    }
-
-    @Test
-    public void testPlayer1Wins() throws TennisGameException {
-        game.player1Scored();
-        game.player1Scored();
-        game.player1Scored();
-        game.player1Scored();
-
-        assertEquals("Player1 should win", "player1 wins", game.getScore());
-    }
-
-    @Test
-    public void testPlayer2Wins() throws TennisGameException {
-        game.player2Scored();
-        game.player2Scored();
-        game.player2Scored();
-        game.player2Scored();
-
-        assertEquals("Player2 should win", "player2 wins", game.getScore());
-    }
-
-    @Test
-    public void testGetScoreDeuce() throws TennisGameException {
-        game.player1Scored();
-        game.player2Scored();
-        game.player1Scored();
-        game.player2Scored();
-        game.player1Scored();
-        game.player2Scored();
-        game.player2Scored(); // Deuce
-
-        assertEquals("The score should be 'deuce'", "deuce", game.getScore());
-    }
-    
-    
-    @Test
-    public void testPlayer2Scored() throws TennisGameException {
-        game.player2Scored();
-        assertEquals("Score should be 'love - 15'", "love - 15", game.getScore());
-    }
-    
-    
-    
-    @Test
-    public void testGetScorePlayer1Wins() throws TennisGameException {
-        game.player1Scored();
-        game.player1Scored();
-        game.player1Scored();
-        game.player1Scored();
-        assertEquals("Player 1 should win", "player1 wins", game.getScore());
-    }
-    
-    
-
-
-    
-    
-    
-    
-    
-    @Test
-    public void testGetScorePlayer2Advantage() throws TennisGameException {
-        game.player1Scored();
-        game.player1Scored();
-        game.player1Scored();
-        game.player2Scored();
-        game.player2Scored();
-        game.player2Scored();
-        game.player2Scored(); // Player 2 has advantage
-        assertEquals("Player 2 should have advantage", "player2 has advantage", game.getScore());
-    }
-
-    
-    @Test
-    public void testGetScorePlayer1Advantage() throws TennisGameException {
-        game.player1Scored();
-        game.player1Scored();
-        game.player1Scored();
-        game.player2Scored();
-        game.player2Scored();
-        game.player2Scored();
-        game.player1Scored(); // Player 1 has advantage
-        assertEquals("Player 1 should have advantage", "player1 has advantage", game.getScore());
-    }
-    
-    
-    
-    @Test
-    public void testAdvantage() throws TennisGameException {
-        game.player1Scored();
-        game.player2Scored();
-        game.player1Scored();
-        game.player2Scored();
-        game.player1Scored();
-        game.player2Scored();
-        game.player1Scored();
-        game.player1Scored();
-
-       assertEquals("Player1 should have advantage", "player1 has advantage", game.getScore());
-    }
-
-    
-
-    @Test
-    public void testPlayer2ScoredGameEnded() throws TennisGameException {
-        game.player1Scored();
-        game.player1Scored();
-        game.player1Scored();
-        game.player1Scored();
-        // Player 1 has won, the game is over
-
-        try {
-            game.player2Scored(); // This should throw an exception
-            fail("Expected TennisGameException");
-        } catch (TennisGameException e) {
-            // This is expected
-        }
-    }
-    
-    
-    @Test
-    public void testGetScorePlayer2Wins() throws TennisGameException {
-        game.player2Scored();
-        game.player2Scored();
-        game.player2Scored();
-        game.player2Scored();
-        assertEquals("Player 2 should win", "player2 wins", game.getScore());
-    }
-
-    
-    
-    
-    
-    
-    @Test
-    public void testInvalidScoring() {
-        try {
-            game.player1Scored();
-            game.player1Scored();
-            game.player1Scored();
-            game.player1Scored();
-            game.player1Scored(); // This should throw an exception
-            fail("Expected TennisGameException");
-        } catch (TennisGameException e) {
-            // This is expected
-        }
-    }
-}
-
-
-*/
